17976b8505e1138822458d142a242021360054f4
Klod <c.buisson@it-student.fr>
2021-08-04 22:56:31 +0200

    a little style

--- my_app/lib/my_app_web/controllers/page_controller.ex
+++ my_app/lib/my_app_web/controllers/page_controller.ex
@@ -4,9 +4,8 @@ defmodule MyAppWeb.PageController do
 
   def index(conn, _param) do
     champ = Characters.fetch_characters["champions"]
-    lists = Enum.uniq( Enum.flat_map(champ, fn(row)-> row["tags"]end),fn x -> [x , x] end)
-    # lists
-    # |> Poison.decode!
+    lists = Enum.uniq_by( Enum.flat_map(champ, fn(row)-> row["tags"]end),fn x -> [x , x] end)
+    
 
     render(conn, "index.html", champions: champ, list: lists)
 
